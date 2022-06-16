7f81848ef6c6cc0a08301a66d17f8639f57fdb53
Jun <jun1781@naver.com>
2022-06-16 17:39:03 +0900

    second

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,38 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@mui/styles";
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
+				<body>
 				<Head>
 					<link rel="manifest" href="/manifest.json" />
 					<link rel="apple-touch-icon" href="/logo-128x128.png" />
 					<link rel="theme-color" href="#fff" />
 				</Head>
-				<body>
-					<Main />
-					<NextScript />
+				<Main />
+				<NextScript />
 				</body>
 			</Html>
 		);
 	}
 }
 
-export default MyDocument;
+MyDocument.getInitialProps = async ctx => {
+	const materialSheets = new ServerStyleSheets();
+	const originalRenderPage = ctx.renderPage;
+
+	ctx.renderPage = () =>
+		originalRenderPage({
+			enhanceApp: App => props => materialSheets.collect(<App {...props} />)
+		});
+
+	const initialProps = await Document.getInitialProps(ctx);
+	return {
+		...initialProps,
+		styles: <>{initialProps.styles}</>
+	};
+};
+
